998ed2d1abaeaedefdb2f996f6ae772755477a98
Samuel Hinshelwood Jr <samhinsh@gmail.com>
2016-03-16 17:07:46 -0700

    Added email attachment functionality

--- Scripts/SendEmails.py
+++ Scripts/SendEmails.py
@@ -2,16 +2,41 @@
 # This file describes functions for sending emails programmatically.  
 
 import smtplib #standard email module
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
 import Credentials
+
+# Message Send Config
 
 FROM = Credentials.FROM
 TO = [Credentials.TO] # must be a list
+TO = ", ".join(TO)
 
 SUBJECT = "DoorCeptionist Doormail Alert"
 
 BODY = "This message was sent from your DoorCeptionist Device in " + Credentials.LOCATION + "\n" \
 	   "Please see the attached message from your guest: "
 
+FILENAME = "Visitor Message.wav" # for email name scheme
+ATTACHMENT = open("/Users/samuelhinshelwood/Downloads/foobar.wav", "rb") # for finding file on computer
+
+# Prepare email config & body
+
+message = MIMEMultipart()
+message['From'] = FROM
+message['To'] = TO
+message['Subject'] = SUBJECT
+message.attach(MIMEText(BODY, 'plain'))
+
+# prepare email attachment (encoding)
+
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((ATTACHMENT).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % FILENAME)
+message.attach(part)
 
 # Login to email server
 
@@ -19,17 +44,9 @@ server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(Credentials.USERNAME, Credentials.PASSWORD)
 
-# Prepare actual email
-
-message = """\
-From: %s
-To: %s
-Subject: %s
-
-%s
-""" % (FROM, ", ".join(TO), SUBJECT, BODY)
+messageText = message.as_string()
 
 # Send the email
 
-server.sendmail(FROM, TO, message)
+server.sendmail(FROM, TO, messageText)
 server.quit()
